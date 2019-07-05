83b19b40553e3bd0f5b2f1ac7ade66a33254056d
Ujwal P <ujwalp09@gmail.com>
2019-07-05 17:54:05 +0530

    sepolicy: common: inherit dynamic sepolicy
    This was added for fod, but never inherited in aosip since we havent used it before.
    
    Change-Id: Iac1aee8fc006ff1d00301b765333102147461d17

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,11 +3,23 @@
 # inherit from Stag-OS
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/stag/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/stag/sepolicy/common/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/stag/sepolicy/common/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/stag/sepolicy/common/dynamic \
     device/stag/sepolicy/common/vendor
+endif
